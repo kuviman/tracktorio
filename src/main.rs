ac7bc97e00488986c70f865d55291b4b6a8d7100
kuviman <kuviman@gmail.com>
2025-03-22 12:10:07 +0400

    config

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
 use geng::prelude::*;
 
+#[derive(Deserialize)]
+struct Config {
+    background: Rgba<f32>,
+    fov: f32,
+}
+
 struct Game {
     geng: Geng,
+    camera: Camera2d,
+    config: Config,
 }
 
 impl Game {
     async fn new(geng: &Geng) -> Self {
-        Self { geng: geng.clone() }
+        let config: Config = file::load_detect(run_dir().join("assets").join("config.toml"))
+            .await
+            .unwrap();
+        Self {
+            geng: geng.clone(),
+            camera: Camera2d {
+                center: vec2::ZERO,
+                rotation: Angle::ZERO,
+                fov: Camera2dFov::MinSide(config.fov),
+            },
+            config,
+        }
     }
 }
 
 impl geng::State for Game {
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
-        ugli::clear(framebuffer, Some(Rgba::BLACK), None, None);
+        ugli::clear(framebuffer, Some(self.config.background), None, None);
     }
 }
 
